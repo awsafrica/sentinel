15a4133efd082b90eea3128bfe5f733fc74995ad
SA Blockchain Solutions <32270605+awsafrica@users.noreply.github.com>
2018-07-27 12:31:59 +0200

    Update travis_setup.sh

--- share/travis_setup.sh
+++ share/travis_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -evx
 
-mkdir ~/.dashcore
+mkdir ~/.brixcoincore
 
 # safety check
-if [ ! -f ~/.dashcore/.dash.conf ]; then
-  cp share/dash.conf.example ~/.dashcore/dash.conf
+if [ ! -f ~/.brixcoincore/.dash.conf ]; then
+  cp share/brixcoin.conf.example ~/.brixcoincore/brixcoin.conf
 fi
